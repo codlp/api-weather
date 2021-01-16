d37f44ea6fb3c1bdf71ee121d9572c4f2295c0e2
coralie delpha <coralie.delpha@gmail.com>
2021-01-16 23:53:16 +0100

    locations controller

---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::LocationsController < ApplicationController
+    before_action :set_location
+
+    # What we request for a specific location temperature
+    def show
+        render json: {
+            id: @location.id,
+            name: @location.name
+        }
+    end
+
+    private
+
+        def set_location
+            @location = Location.find(params[:id])
+        end
+end
